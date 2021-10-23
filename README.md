3742738a9154d46976dfe74da0bde978c2245cb9
David Sims <thedavesims@users.noreply.github.com>
2021-10-24 00:55:26 +0100

    Added mouse pointer and buttons

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# wasm4-user-input
- A cart that dispays user input from WASM-4
+# WASM-4 User Input Example
+ A work-in-progress cart that displays user input from WASM-4.
---
+++ src/main.c
@@ -0,0 +1,46 @@
+#include "wasm4.h"
+
+#define MOUSE_MEMORY 0x001a;
+
+struct mouse_data {
+    int16_t x;
+    int16_t y;
+    uint8_t buttons;
+};
+
+const uint8_t cursor[] = {
+    0b00000011,
+    0b00000011,
+    0b00001111,
+    0b00000111,
+    0b00100011,
+    0b00110001,
+    0b11111000,
+    0b11111100,
+};
+
+void update () {
+
+    static struct mouse_data* m = (struct mouse_data*)MOUSE_MEMORY;
+
+    *DRAW_COLORS = 3;
+    text("Mouse Test", 10, 10);
+
+    *DRAW_COLORS = 2;
+    if(m->buttons & 0x1)
+    {
+        text("Left Button", 10, 20);
+    }
+
+    if(m->buttons & 0x2)
+    {
+        text("Right Button", 10, 30);
+    }
+
+    if(m->buttons & 0x4)
+    {
+        text("Middle Button", 10, 40);
+    }
+
+    blit(cursor, m->x, m->y, 8, 8, BLIT_1BPP);
+}
